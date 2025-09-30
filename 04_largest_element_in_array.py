d3f255d3f3f7ddf3b6ba3efc7401b26b42424c5b
prabhatdhadse <prabhatdhadse1204@gmail.com>
2025-09-30 17:04:18 +0530

    solved

---
+++ 04_largest_element_in_array.py
@@ -0,0 +1,26 @@
+nums = [55,32,-98,97,3,67]
+
+largest = nums[0]
+n = len(nums)
+for i in range(0,n):
+    largest = max(largest,nums[i])
+print(largest)
+
+
+
+
+# nums = [55,32,-98,97,3,67]
+
+# largest = nums[0]
+# n = len(nums)
+# for i in range(0,n):
+#  if nums[i]> largest:
+#         largest = nums[i]
+# print(largest)
+
+
+
+# nums = [55,32,-98,97,3,67]
+# print(max(nums))
+
+ 
